84227f6a232d198f4dbb908df2e8116b01c85192
LilithVinter <108224035+LilithVinter@users.noreply.github.com>
2024-08-25 17:50:01 +0200

    simple styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,10 +4,11 @@ export default function Meaning(props) {
   console.log(props.meaning);
 
   return (
-    <div className="defintionContainer">
+    <div className="definitionContainer">
       <h3> {props.meaning.partOfSpeech}</h3>
       <p className="definitionText">{props.meaning.definition}</p>
       <br />
+      <strong> example:</strong>
       <em className="example"> {props.meaning.example} </em>
     </div>
   );
